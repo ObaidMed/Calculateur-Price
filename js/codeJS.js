ff8890bca81f5e6b63eb24f6360bf842261c233a
ObaidMed <96299663+ObaidMed@users.noreply.github.com>
2022-01-13 23:28:52 +0100

    Calculateur Price

---
+++ js/codeJS.js
@@ -0,0 +1,250 @@
+
+  
+
+
+/**Obaid*Function*/
+/*********cas-lawla********* */
+function reset1() {
+    const inputField = document.getElementById("ca1" );
+    const btn = document.getElementById("btn");
+    btn.addEventListener('click',()=>{
+        // clearing the input field
+        inputField.value = " ";
+    })
+}
+function reset2() {
+    const inputField = document.getElementById("ca2" );
+    const btn = document.getElementById("btn2");
+    btn.addEventListener('click',()=>{
+        // clearing the input field
+        inputField.value = " ";
+    })
+}
+function reset3() {
+    const inputField = document.getElementById("ca3" );
+    const btn = document.getElementById("btn3");
+    btn.addEventListener('click',()=>{
+        // clearing the input field
+        inputField.value = " ";
+    })
+}
+function reset4() {
+    const inputField = document.getElementById("ca4" );
+    const btn = document.getElementById("btn4");
+    btn.addEventListener('click',()=>{
+        // clearing the input field
+        inputField.value = " ";
+    })
+}
+
+/*********cas-lawla********* */
+
+
+/*********cas-2********* */
+function reset5() {
+    const inputField = document.getElementById("ca5" );
+    const btn = document.getElementById("btn5");
+    btn.addEventListener('click',()=>{
+        // clearing the input field
+        inputField.value = " ";
+    })
+}
+function reset6() {
+    const inputField = document.getElementById("ca6" );
+    const btn = document.getElementById("btn6");
+    btn.addEventListener('click',()=>{
+        // clearing the input field
+        inputField.value = " ";
+    })
+}
+function reset7() {
+    const inputField = document.getElementById("ca7" );
+    const btn = document.getElementById("btn7");
+    btn.addEventListener('click',()=>{
+        // clearing the input field
+        inputField.value = " ";
+    })
+}
+function reset8() {
+    const inputField = document.getElementById("ca8" );
+    const btn = document.getElementById("btn8");
+    btn.addEventListener('click',()=>{
+        // clearing the input field
+        inputField.value = " ";
+    })
+}
+
+
+/*********cas-2********* */
+
+
+/*********cas-3********* */
+function reset9() {
+    const inputField = document.getElementById("ca9" );
+    const btn = document.getElementById("btn9");
+    btn.addEventListener('click',()=>{
+        // clearing the input field
+        inputField.value = " ";
+    })
+}
+function reset10() {
+    const inputField = document.getElementById("ca10" );
+    const btn = document.getElementById("btn10");
+    btn.addEventListener('click',()=>{
+        // clearing the input field
+        inputField.value = " ";
+    })
+}
+function reset11() {
+    const inputField = document.getElementById("ca11" );
+    const btn = document.getElementById("btn11");
+    btn.addEventListener('click',()=>{
+        // clearing the input field
+        inputField.value = " ";
+    })
+}
+function reset12() {
+    const inputField = document.getElementById("ca12" );
+    const btn = document.getElementById("btn12");
+    btn.addEventListener('click',()=>{
+        // clearing the input field
+        inputField.value = " ";
+    })
+}
+
+
+
+
+
+/*********cas-3********* */
+  
+
+/************************************ */
+
+function calc1() {
+    var Text = document.getElementById("ca1").value;
+    var price = 1300 ;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+   
+
+}
+
+function calc2() {
+    var Text = document.getElementById("ca2").value;
+    var price = 1100;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+
+}
+function calc3() {
+    var Text = document.getElementById("ca3").value;
+    var price = 1050;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+
+}
+function calc4() {
+    var Text = document.getElementById("ca4").value;
+    var price = 1000;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+
+}
+function calc5() {
+    var Text = document.getElementById("ca5").value;
+    var price = 1050;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+
+}
+
+  /*b2*/ 
+  function calc6() {
+    var Text = document.getElementById("ca6").value;
+    var price = 300 ;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+
+} 
+function calc7() {
+    var Text = document.getElementById("ca7").value;
+    var price = 300 ;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+
+} 
+function calc8() {
+    var Text = document.getElementById("ca8").value;
+    var price = 300 ;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+
+} 
+function calc9() {
+    var Text = document.getElementById("ca9").value;
+    var price = 300 ;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+
+} 
+function calc10() {
+    var Text = document.getElementById("ca10").value;
+    var price = 300 ;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+
+} 
+
+  /*b3*/ 
+  function calc11() {
+    var Text = document.getElementById("ca11").value;
+    var price = 300 ;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+    
+} 
+
+function calc12() {
+    var Text = document.getElementById("ca12").value;
+    var price = 300 ;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+    
+} 
+
+function calc13() {
+    var Text = document.getElementById("ca13").value;
+    var price = 300 ;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+    
+} 
+function calc14() {
+    var Text = document.getElementById("ca14").value;
+    var price = 300 ;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+    
+} 
+function calc15() {
+    var Text = document.getElementById("ca15").value;
+    var price = 300 ;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+    
+} 
+function calc16() {
+    var Text = document.getElementById("ca16").value;
+    var price = 300 ;
+    var re = (price * Text)
+    window.counter.innerHTML = re;
+    
+} 
+
+
+
+
+
+
+
